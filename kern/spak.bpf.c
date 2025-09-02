cfdae73e4333bfefa1e06bc5c36b57f80c5a80e5
Aminassadi <aminassadi.og@gmail.com>
2025-09-03 00:01:31 +0330

    chore: project structure

---
+++ kern/spak.bpf.c
@@ -0,0 +1,13 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+SEC("xdp")
+int xdp_ingress(struct xdp_md *ctx)
+{
+    return XDP_PASS;
+}
+
+
