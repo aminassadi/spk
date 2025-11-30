fdc34b2f1514bd0002142432d73bbf5d7ac6600c
Aminassadi <aminassadi.og@gmail.com>
2025-11-30 11:06:31 +0330

    feat: improve TCP option handling and digest logic in eBPF program, adding validation checks and alignment for options

--- kern/client/spak.client.bpf.c
+++ kern/client/spak.client.bpf.c
@@ -2,8 +2,10 @@
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-#include "../siphash.h"
+#include <strings.h>
 #include <string.h>
+#include "../siphash.h"
+
 
 #define TC_ACT_UNSPEC	(-1)
 #define TC_ACT_OK		0
@@ -22,6 +24,7 @@
 #define IP_MF 0x2000
 #endif
 #define IPPROTO_FRAGMENT 44
+#define MAX_OPTIONS_SIZE 64
 
 #ifndef unlikely
 #define unlikely(x)	__builtin_expect(!!(x), 0)
@@ -62,32 +65,112 @@ struct tcp_opt_spa {
     uint8_t  tag[8];      // SipHash output bytes
 } __attribute__((packed));
 
-__attribute__((always_inline)) static inline void add_digest(struct tcphdr* tcph, struct __sk_buff *ctx)
+
+
+__attribute__((always_inline)) static inline void add_digest(int ip_header_offset, bool is_ipv6, int tcp_header_offset, struct __sk_buff *ctx)
 {
     // Add your digest logic here
     bpf_printk("adding digest\n");
-    void* opt_ptr = (__u8*)tcph + sizeof(struct tcphdr);
-    void* opt_end = (__u8*)tcph + (tcph->doff * 4);
-    void* data_end = (void*)(__u64)ctx->data_end;
 
-    if (unlikely(opt_end > data_end)) {
-        bpf_printk("malformed packet\n");
-        return;
-    }
-    if (unlikely(opt_ptr >= data_end)) {
-        bpf_printk("malformed packet\n");
-        return; // No options present
-    }
    // struct tcp_opt_spa* spa_opt = (struct tcp_opt_spa*)opt_ptr;
     // Grow the packet by the size of struct tcp_opt_spa at the tail
-    int spa_opt_size = sizeof(struct tcp_opt_spa);
+    void* data_end = (void*)(__u64)ctx->data_end;
+    struct tcphdr* tcph = (struct tcphdr*)((void *)(__u64)ctx->data + tcp_header_offset);
+    if (unlikely((void*)tcph + sizeof(struct tcphdr) > data_end))
+    {
+        bpf_printk("malformed packet1\n");
+        return ;
+    }
+    void* opt_ptr = (__u8*)tcph + sizeof(struct tcphdr);
+    void* opt_end = (__u8*)tcph + (tcph->doff * 4);
+   
+    int spa_opt_size = sizeof(struct tcp_opt_spa) + 2; //two nop for alignment
+    __u32 current_options_len = opt_end - opt_ptr;
     int ret = bpf_skb_change_tail(ctx, ctx->len + spa_opt_size, 0);
     if (ret < 0) {
         bpf_printk("failed to grow skb: %d\n", ret);
+        return ;
+    }
+    tcph = (struct tcphdr*)((void *)(__u64)ctx->data + tcp_header_offset);
+    data_end = (void*)(__u64)ctx->data_end;
+    if (unlikely((void*)tcph + sizeof(struct tcphdr) > data_end))
+    {
+        bpf_printk("malformed packet1\n");
+        return ;
+    }
+   
+    opt_ptr = (__u8*)tcph + sizeof(struct tcphdr);
+    opt_end = (__u8*)tcph + (tcph->doff * 4);
+    if (unlikely(opt_end > data_end)) {
+        bpf_printk("malformed packet2\n");
+        return ;
+    }
+
+    __u32 rem_size = opt_end - opt_ptr;
+    if (rem_size > MAX_OPTIONS_SIZE) {
+        bpf_printk("options size too large\n");
         return;
     }
-    memcpy(, )
- 
+    #pragma unroll
+    for(int i = MAX_OPTIONS_SIZE; i >= 0; i--) {
+        if (i > rem_size)
+            continue;
+        asm volatile("%[i] &= 63\n" : [i] "+&r"(i));
+        if (unlikely(opt_ptr + i + spa_opt_size >= data_end )) {
+            bpf_printk("malformed packet3\n");
+            continue;
+        }
+        *(__u8*)(opt_ptr + i + spa_opt_size) = *(__u8*)(opt_ptr + i);
+    }
+   
+    struct tcp_opt_spa* spa_opt = (struct tcp_opt_spa*)opt_ptr;
+    if (unlikely(opt_ptr + sizeof(struct tcp_opt_spa) > data_end)) {
+        bpf_printk("malformed packet4\n");
+        return;
+    }
+    spa_opt->kind = 253;
+    spa_opt->len = sizeof(struct tcp_opt_spa);
+    spa_opt->exid = bpf_htons(1);
+    spa_opt->ver = 1;
+
+    __u8* nop_ptr1 = opt_ptr + sizeof(struct tcp_opt_spa);
+    __u8* nop_ptr2 = opt_ptr + sizeof(struct tcp_opt_spa) + 1;
+
+    if(unlikely(nop_ptr1 + 3 > data_end)){
+        bpf_printk("malformed packet5\n");
+        return;
+    }
+    *nop_ptr1 = 1;
+    *nop_ptr2 = 1;
+
+
+    //recalculate checksums
+    tcph->doff+= spa_opt_size / 4;
+    struct iphdr* iph = NULL;
+    struct ipv6hdr* ip6h = NULL;
+    if(is_ipv6) {
+        ip6h = (struct ipv6hdr*)((void *)(__u64)ctx->data + ip_header_offset);
+        if(unlikely((void*)ip6h + sizeof(struct ipv6hdr) > data_end)) {
+            bpf_printk("malformed packet6\n");
+            return;
+        }
+        ip6h->payload_len = bpf_htons(bpf_ntohs(ip6h->payload_len) + spa_opt_size);
+        bpf_l3_csum_replace(ctx, ip_header_offset, 0, 0, sizeof(struct ipv6hdr));
+    }
+    else {
+        iph = (struct iphdr*)((void *)(__u64)ctx->data + ip_header_offset);
+        if(unlikely((void*)iph + sizeof(struct iphdr) > data_end)) {
+            bpf_printk("malformed packet6\n");
+            return;
+        }
+        iph->tot_len =  bpf_htons(bpf_ntohs(iph->tot_len) + spa_opt_size);
+        bpf_l3_csum_replace(ctx, ip_header_offset, 0, 0, sizeof(struct iphdr));
+    }
+
+    bpf_l4_csum_replace(ctx, tcp_header_offset, 0, 0, sizeof(struct tcphdr));
+
+    bpf_printk("end");
+
 }
 SEC("classifier/egress")
 int tc_egress(struct __sk_buff *ctx)
@@ -119,7 +202,8 @@ int tc_egress(struct __sk_buff *ctx)
     }
     struct iphdr* iph = NULL;
     struct ipv6hdr* ip6h = NULL;
-
+    int ip_header_offset = pkt_len;
+    bool is_ipv6 = false;
     bool is_fragment = false;
     struct flow current_flow;
     memset(&current_flow, 0, sizeof(current_flow));
@@ -140,6 +224,7 @@ int tc_egress(struct __sk_buff *ctx)
     }
     else if (bpf_ntohs(eth->h_proto) == ETH_P_IPV6)
     {
+        is_ipv6 = true;
         ip6h = data + pkt_len;
         pkt_len += sizeof(struct ipv6hdr);
         if (unlikely(data + pkt_len > data_end))
@@ -175,10 +260,10 @@ int tc_egress(struct __sk_buff *ctx)
         return TC_ACT_SHOT;
     }
 
-    current_flow.dst_port = tcp_hdr->dest;
-    current_flow.src_port = tcp_hdr->source;
+    // current_flow.dst_port = tcp_hdr->dest;
+    // current_flow.src_port = tcp_hdr->source;
     if (tcp_hdr->syn && !tcp_hdr->ack) {
-        add_digest(tcp_hdr, ctx);
+        add_digest(ip_header_offset, is_ipv6, pkt_len, ctx);
         return TC_ACT_OK;
     }
     // Example: Log that we're processing egress traffic
