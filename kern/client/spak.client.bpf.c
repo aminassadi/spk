ac9b2849138d43c704599e10a6c431c25ad46cde
Aminassadi <aminassadi.og@gmail.com>
2025-12-02 16:14:37 +0330

    feat: enhance TCP checksum calculation wip: refactoring

--- kern/client/spak.client.bpf.c
+++ kern/client/spak.client.bpf.c
@@ -25,6 +25,8 @@
 #endif
 #define IPPROTO_FRAGMENT 44
 #define MAX_OPTIONS_SIZE 64
+#define MAX_TCP_LEN 256
+
 
 #ifndef unlikely
 #define unlikely(x)	__builtin_expect(!!(x), 0)
@@ -65,7 +67,83 @@ struct tcp_opt_spa {
     uint8_t  tag[8];      // SipHash output bytes
 } __attribute__((packed));
 
+static __always_inline __sum16 csum_fold(__wsum csum) {
+    csum = (csum & 0xffff) + (csum >> 16);
+    csum = (csum & 0xffff) + (csum >> 16);
+    return (__sum16)~csum;
+  }
 
+  __attribute__((__always_inline__)) static inline __u16
+  csum_fold_helper(__u64 csum) {
+    int i;
+  #pragma unroll
+    for (i = 0; i < 4; i++) {
+      if (csum >> 16)
+        csum = (csum & 0xffff) + (csum >> 16);
+    }
+    return ~csum;
+  }
+
+static __always_inline __u16 csum_tcpudp_ip(__u32 saddr, __u32 daddr,
+    __u32 len, __u8 proto,
+    __u32 csum) {
+        __u64 s = csum;
+
+        s += (__u32)saddr;
+        s += (__u32)daddr;
+      #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        s += proto + len;
+      #elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        s += (proto + len) << 8;
+      #else
+      #error Unknown endian
+      #endif
+        s = (s & 0xffffffff) + (s >> 32);
+        s = (s & 0xffffffff) + (s >> 32);
+      
+        return csum_fold((__u32)s);
+}
+
+static __always_inline __u16 csum_tcpudp_ipv6(const struct in6_addr *saddr,
+  const struct in6_addr *daddr,
+  __u32 len, __u8 proto,
+  __u32 csum) {
+    __u64 sum = csum;
+    int i;
+
+    #pragma unroll
+    for (i = 0; i < 4; i++)
+    sum += (__u32)saddr->in6_u.u6_addr32[i];
+
+    #pragma unroll
+    for (i = 0; i < 4; i++)
+    sum += (__u32)daddr->in6_u.u6_addr32[i];
+
+    /* Don't combine additions to avoid 32-bit overflow. */
+    sum += bpf_htonl(len);
+    sum += bpf_htonl(proto);
+
+    sum = (sum & 0xffffffff) + (sum >> 32);
+    sum = (sum & 0xffffffff) + (sum >> 32);
+
+    return csum_fold((__u32)sum);
+}
+
+__attribute__((__always_inline__)) static inline void
+ipv4_csum_inline(void *iph, __u64 *csum) {
+  __u16 *next_iph_u16 = (__u16 *)iph;
+#pragma unroll
+  for (int i = 0; i < sizeof(struct iphdr) >> 1; i++) {
+    *csum += *next_iph_u16++;
+  }
+  *csum = csum_fold_helper(*csum);
+}
+
+__attribute__((__always_inline__)) static inline void
+ipv4_csum(void* data_start, int data_size, __u64* csum) {
+  *csum = bpf_csum_diff(0, 0, data_start, data_size, *csum);
+  *csum = csum_fold_helper(*csum);
+}
 
 __attribute__((always_inline)) static inline void add_digest(int ip_header_offset, bool is_ipv6, int tcp_header_offset, struct __sk_buff *ctx)
 {
@@ -86,7 +164,9 @@ __attribute__((always_inline)) static inline void add_digest(int ip_header_offse
    
     int spa_opt_size = sizeof(struct tcp_opt_spa) + 2; //two nop for alignment
     __u32 current_options_len = opt_end - opt_ptr;
+   
     int ret = bpf_skb_change_tail(ctx, ctx->len + spa_opt_size, 0);
+ //   int ret = bpf_skb_change_tail(ctx, 512, 0);
     if (ret < 0) {
         bpf_printk("failed to grow skb: %d\n", ret);
         return ;
@@ -113,7 +193,7 @@ __attribute__((always_inline)) static inline void add_digest(int ip_header_offse
     }
     #pragma unroll
     for(int i = MAX_OPTIONS_SIZE; i >= 0; i--) {
-        if (i > rem_size)
+        if (i > rem_size-1)
             continue;
         asm volatile("%[i] &= 63\n" : [i] "+&r"(i));
         if (unlikely(opt_ptr + i + spa_opt_size >= data_end )) {
@@ -143,35 +223,53 @@ __attribute__((always_inline)) static inline void add_digest(int ip_header_offse
     *nop_ptr1 = 1;
     *nop_ptr2 = 1;
 
+    tcph->doff += spa_opt_size / 4;
+    tcph->check = 0;
+   
+    __u32 tcp_len = tcph->doff * 4;
+    bpf_printk("tcp_len: %d\n", tcp_len);
+    if(unlikely((void*)tcph + tcp_len > data_end )) {
+        bpf_printk("malformed packet7\n");
+        return;
+    }
+ 
+    asm volatile("%[tcp_len] &= 63\n" : [tcp_len] "+&r"(tcp_len));
 
-    //recalculate checksums
-    tcph->doff+= spa_opt_size / 4;
-    struct iphdr* iph = NULL;
-    struct ipv6hdr* ip6h = NULL;
+   __u32 value = bpf_csum_diff(0, 0, (void*)tcph, sizeof(struct tcphdr), 0);
+   if(unlikely(opt_ptr + 40 > data_end)) {
+    bpf_printk("malformed packet8\n");
+    return;
+   }
+   value = bpf_csum_diff(0, 0, (void*)opt_ptr, 40, value);
+
+
     if(is_ipv6) {
-        ip6h = (struct ipv6hdr*)((void *)(__u64)ctx->data + ip_header_offset);
+        struct ipv6hdr* ip6h = (struct ipv6hdr*)((void *)(__u64)ctx->data + ip_header_offset);
         if(unlikely((void*)ip6h + sizeof(struct ipv6hdr) > data_end)) {
             bpf_printk("malformed packet6\n");
             return;
         }
-        ip6h->payload_len = bpf_htons(bpf_ntohs(ip6h->payload_len) + spa_opt_size);
-        bpf_l3_csum_replace(ctx, ip_header_offset, 0, 0, sizeof(struct ipv6hdr));
-    }
+     tcph->check = csum_tcpudp_ipv6(&ip6h->saddr, &ip6h->daddr, tcp_len, IPPROTO_TCP, value);
+    } 
     else {
-        iph = (struct iphdr*)((void *)(__u64)ctx->data + ip_header_offset);
+        struct iphdr* iph = (struct iphdr*)((void *)(__u64)ctx->data + ip_header_offset);
         if(unlikely((void*)iph + sizeof(struct iphdr) > data_end)) {
-            bpf_printk("malformed packet6\n");
+            bpf_printk("malformed packet4\n");
             return;
         }
+        bpf_printk("tcp->check: %d\n", tcph->check);
         iph->tot_len =  bpf_htons(bpf_ntohs(iph->tot_len) + spa_opt_size);
-        bpf_l3_csum_replace(ctx, ip_header_offset, 0, 0, sizeof(struct iphdr));
+        u64 csum = 0;
+        iph->check = 0;
+        ipv4_csum((void*)iph, sizeof(struct iphdr), &csum);
+        iph->check = csum;
+        tcph->check = csum_tcpudp_ip(iph->saddr, iph->daddr, tcp_len, IPPROTO_TCP, value);
+        bpf_printk("after: tcp->check: %d\n", tcph->check);
     }
 
-    bpf_l4_csum_replace(ctx, tcp_header_offset, 0, 0, sizeof(struct tcphdr));
-
     bpf_printk("end");
-
 }
+
 SEC("classifier/egress")
 int tc_egress(struct __sk_buff *ctx)
 {
